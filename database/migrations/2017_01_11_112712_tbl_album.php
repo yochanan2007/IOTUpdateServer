4ac6c65f00eb06ed196deeb5fec9124db1e9c442
yochanandjbb@github.com <yochanan2007@gmail.com>
2017-01-11 14:47:41 +0200

    Initial commit

---
+++ database/migrations/2017_01_11_112712_tbl_album.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TblAlbum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('album', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('album');
+    }
+}
---
+++ public/test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yochanand
+ * Date: 1/11/2017
+ * Time: 2:38 PM
+ */
+
+
+
